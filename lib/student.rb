d056c58d7c4a101ce6581c0d22194033d288efca
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-10-08 14:37:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,9 @@ class Student
   end
 
   def self.all
-    sql = <<-SQL
+    sql = <<-bob
     SELECT * FROM students
-    SQL
+    bob
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
