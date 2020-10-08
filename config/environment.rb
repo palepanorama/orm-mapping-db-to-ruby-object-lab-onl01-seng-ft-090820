6028ff43b6c85a61845b449311af6408b096b584
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-10-08 14:50:10 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,5 @@ require 'sqlite3'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
-DB.results_as_hash = true 
+DB2 = SQLite3::Database.new("db/test.db")
+DB2.results_as_hash = true 
